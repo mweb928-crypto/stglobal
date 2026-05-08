e47c8474ce22ed94e6644a09043a767ac95914f9
yung blazk <ttadoc38@gmail.com>
2026-05-08 09:04:07 +0700

    Fix all build errors for Vercel

--- api/trpc.ts
+++ api/trpc.ts
@@ -1,16 +1,1 @@
-﻿import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import express from "express";
-import { appRouter } from "../server/routers";
-import { createContext } from "../server/_core/context";
-
-const app = express();
-app.use(express.json());
-app.use(
-  "/api/trpc",
-  createExpressMiddleware({
-    router: appRouter,
-    createContext,
-  })
-);
-
-export default app;
+import express from "express"; import { createExpressMiddleware } from "@trpc/server/adapters/express"; import cors from "cors"; import { appRouter } from "../server/routers"; import { createContext } from "../server/_core/context"; const app = express(); app.use(cors()); app.use(express.json()); app.use("/", createExpressMiddleware({ router: appRouter, createContext })); export default app;
